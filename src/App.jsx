012d92f90538f918ddfe1afedfc322d3ee0d2a89
John Williams <jhw13@email.uscb.edu>
2026-03-29 15:46:21 -0400

    Implementededit, view, and add user.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Navbar from './Layout/Navbar'
-import Home from './Pages/Home'
+import React from "react";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser/>}/>
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+        </Routes>
+      </Router>
     </div>
-
-    
   );
 }
 
-export default App
+export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
 
     const [users, setUsers] = useState([])
+
+const { id } = useParams();   // <-- FIXED
 
 useEffect(() => {
     loadUsers();
@@ -13,6 +17,11 @@ useEffect(() => {
         const result = await axios.get("http://localhost:8080/users")
         setUsers(result.data);
     }
+
+const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/user/${id}`);
+    loadUsers();
+}
 
   return (
     <div className='container-fluid'>
@@ -36,7 +45,21 @@ useEffect(() => {
       <td>{user.name}</td>
       <td>{user.userName}</td>
       <td>{user.email}</td>
-      <td>@mdo</td>
+      <td>
+        
+        <Link 
+                className="btn btn-primary mx-2"
+                to={`/viewuser/${user.id}`}>
+              View
+              </Link>
+        <Link className='btn btn-outline-primary mx-2' to={`/edit/${user.id}`}>
+          Edit
+        </Link>
+        <button className='btn btn-danger mx-2' 
+        onClick={() => deleteUser(user.id)}>
+          Delete
+        </button>
+      </td>
     </tr>
   </tbody>
     ))}
